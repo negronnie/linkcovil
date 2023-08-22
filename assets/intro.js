3f1f9297ad50a4631657d6e8696b6c7c04ccf884
Lucas Ponciano <negronnie@negronnie.local>
2023-08-22 13:07:37 -0300

    ajuste intro web

--- assets/intro.js
+++ assets/intro.js
@@ -1,11 +1,32 @@
-function startIntro (){
+function start (){
     var width = window.innerWidth;
+
+    introIntro = document.getElementById('introCovil')
+
+// switch (width) {
+//     case (width < 1280):
+//         introCovil.playSegments([0, 160],true)
+        
+//         console.log("mobile")
+//         break;
+
+//     default:
+//         logoCovil.playSegments([0, 60],true)
+//         console.log("web")
+//         break;
+// }
+
+
 
     if (width < 1280) {
         introCovil.playSegments([0, 160],true)
-    } else if (width > 1440){
+        console.log("mobile")
+    } else {
+        
         logoCovil.playSegments([0, 60],true)
+        introIntro.classList.add('clearIntro')
+        console.log("web")
     }
 }
 
-startIntro()
+start()
