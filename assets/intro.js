e701d4afbc052e3a1c6d75c6cc21de823bf66e45
Lucas Ponciano <negronnie@negronnie.local>
2023-08-22 13:15:36 -0300

    removido console logs kkkkk

--- assets/intro.js
+++ assets/intro.js
@@ -3,29 +3,11 @@ function start (){
 
     introIntro = document.getElementById('introCovil')
 
-// switch (width) {
-//     case (width < 1280):
-//         introCovil.playSegments([0, 160],true)
-        
-//         console.log("mobile")
-//         break;
-
-//     default:
-//         logoCovil.playSegments([0, 60],true)
-//         console.log("web")
-//         break;
-// }
-
-
-
     if (width < 1280) {
         introCovil.playSegments([0, 160],true)
-        console.log("mobile")
     } else {
-        
         logoCovil.playSegments([0, 60],true)
         introIntro.classList.add('clearIntro')
-        console.log("web")
     }
 }
 
